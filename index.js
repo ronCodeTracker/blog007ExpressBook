52829155bf0dd4d968138606b3542112d349eec6
ronCodeTracker <2fixelectronics777@gmail.com>
2023-10-22 13:55:39 -0500

    another mod

--- index.js
+++ index.js
@@ -4,7 +4,14 @@
 
 
 const express = require('express')
+const path = require('path')
+
+
 const app = new express()
+const ejs = require('ejs')
+app.set('view engine','ejs')
+
+
 
 app.use(express.static('public'))
 
@@ -12,6 +19,44 @@ app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
+app.get('/',(req, res)=>{
+    
+    //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
+    res.json({error: err })
+
+
+
+})
+
+
+app.get('/about',(req, res)=>{
+    
+    //res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
+
+
+
+})
+
+app.get('/post',(req, res)=>{
+    
+    //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
+
+
+
+})
+
+
+app.get('/contact',(req, res)=>{
+    
+    //res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
+
+
+
+})
 
 
 
